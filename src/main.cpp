87c96d76c628bb3e903beea4f28920c81a533779
JP <jp@jplekkerkerker.nl>
2021-06-24 20:37:50 +0200

    WIP: adding generating qr-code & image in code

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,16 @@
 #include <Wire.h>
 #include <ESPmDNS.h>
 #include <cJSON.h>
+#include <qrencode.h>
 
 #include <boards.h>
 #include <GxEPD.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
-#include <Fonts/FreeSans9pt7b.h>
-
+#include <Fonts/FreeSerif18pt7b.h>
+#include <Fonts/FreeSerifBold12pt7b.h>
+#include <Fonts/FreeSansBold12pt7b.h>
+#include <Fonts/FreeSansOblique12pt7b.h>
 
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
@@ -31,7 +34,7 @@
 
 #include "utilities.h"
 #include "defConfig.h"
-
+#include "create_qr.h"
 
 //Select the header file corresponding to the screen.
 
@@ -72,10 +75,19 @@ enum {
     GxEPD_ALIGN_CENTER,
 };
 
+typedef struct {
+    char beername[64];
+    char style[64];
+    char abv[32];
+    char ibu[32];
+    char link[64];
+} Beer_Info_t;
+
 using namespace         ace_button;
 AsyncWebServer          server(80);
 GxIO_Class              io(SPI,  EPD_CS, EPD_DC,  EPD_RSET);
 GxEPD_Class             display(io, EPD_RSET, EPD_BUSY);
+Beer_Info_t             info;
 
 const uint8_t           btns[] = BUTTONS;
 const uint8_t           handle_btn_nums = sizeof(btns) / sizeof(*btns);
@@ -83,7 +95,82 @@ const uint8_t           handle_btn_nums = sizeof(btns) / sizeof(*btns);
 
 
 extern void drawBitmap(GxEPD &display, const char *filename, int16_t x, int16_t y, bool with_color);
+extern void createQrCode(GxEPD &display, String message);
 void showBeerImage(void);
+
+/****************************************************
+  ____                   _____        __
+ |  _ \                 |_   _|      / _|
+ | |_) | ___  ___ _ __    | |  _ __ | |_ ___
+ |  _ < / _ \/ _ \ '__|   | | | '_ \|  _/ _ \
+ | |_) |  __/  __/ |     _| |_| | | | || (_) |
+ |____/ \___|\___|_|    |_____|_| |_|_| \___/
+
+****************************************************/
+void saveBeerInfo(Beer_Info_t *info)
+{
+    File file = FILESYSTEM.open(BEER_CONFIG_FILE_NAME, FILE_WRITE);
+    if (!file) {
+        Serial.println(F("Failed to create file"));
+        return;
+    }
+    cJSON *root =  cJSON_CreateObject();
+    cJSON_AddStringToObject(root, "beername", info->beername);
+    cJSON_AddStringToObject(root, "style", info->style);
+    cJSON_AddStringToObject(root, "abv", info->abv);
+    cJSON_AddStringToObject(root, "ibu", info->ibu);
+    cJSON_AddStringToObject(root, "link", info->link);
+    const char *str =  cJSON_Print(root);
+    file.write((uint8_t *)str, strlen(str));
+    file.close();
+    cJSON_Delete(root);
+}
+
+void loadDefaultInfo(void)
+{
+    strlcpy(info.beername, "Tuinhop",       sizeof(info.beername));
+    strlcpy(info.style,    "DIPA",          sizeof(info.style));
+    strlcpy(info.abv,      "7.2 %",         sizeof(info.abv));
+    strlcpy(info.ibu,      "105 IBU",       sizeof(info.ibu));
+    strlcpy(info.link,     "https://untappd.com/qr/beer/2312755/", sizeof(info.link));
+    saveBeerInfo(&info);
+}
+
+bool loadBeerInfo(Beer_Info_t *info)
+{
+    if (!FILESYSTEM.exists(BEER_CONFIG_FILE_NAME)) {
+        Serial.println("load configure fail");
+        return false;
+    }
+    File file = FILESYSTEM.open(BEER_CONFIG_FILE_NAME);
+    if (!file) {
+        Serial.println("Open Fial -->");
+        return false;
+    }
+    cJSON *root =  cJSON_Parse(file.readString().c_str());
+    if (!root) {
+        return false;
+    }
+    if (cJSON_GetObjectItem(root, "beername")->valuestring) {
+        strlcpy(info->beername, cJSON_GetObjectItem(root, "beername")->valuestring, sizeof(info->beername));
+    }
+    if (cJSON_GetObjectItem(root, "style")->valuestring) {
+        strlcpy(info->style, cJSON_GetObjectItem(root, "style")->valuestring, sizeof(info->style));
+    }
+    if (cJSON_GetObjectItem(root, "abv")->valuestring) {
+        strlcpy(info->abv, cJSON_GetObjectItem(root, "abv")->valuestring, sizeof(info->abv));
+    }
+    if (cJSON_GetObjectItem(root, "ibu")->valuestring) {
+        strlcpy(info->ibu, cJSON_GetObjectItem(root, "ibu")->valuestring, sizeof(info->ibu));
+    }
+    if (cJSON_GetObjectItem(root, "link")->valuestring) {
+        strlcpy(info->link, cJSON_GetObjectItem(root, "link")->valuestring, sizeof(info->link));
+    }
+    file.close();
+    cJSON_Delete(root);
+    return true;
+}
+
 /****************************************************
  __          ___ ______ _
  \ \        / (_)  ____(_)
@@ -101,7 +188,7 @@ void setupWiFi(bool apMode)
         char    apName[64];
         WiFi.mode(WIFI_AP);
         WiFi.macAddress(mac);
-        sprintf(apName, "TTGO-Badge-%02X:%02X", mac[4], mac[5]);
+        sprintf(apName, "TTGO-TapHandle-%02X:%02X", mac[4], mac[5]);
         WiFi.softAP(apName);
     } else {
         WiFi.mode(WIFI_STA);
@@ -154,6 +241,30 @@ static void asyncWebServerFileUploadCb(AsyncWebServerRequest *request, const Str
     }
 }
 
+static void asyncWebServerDataPostCb(AsyncWebServerRequest *request)
+{
+    request->send(200, "text/plain", "");
+    for (int i = 0; i < request->params(); i++) {
+        String name = request->getParam(i)->name();
+        String params = request->getParam(i)->value();
+        Serial.println(name + " : " + params);
+
+        if (name == "beername") {
+            strlcpy(info.beername, params.c_str(), sizeof(info.beername));
+        } else if (name == "style") {
+            strlcpy(info.style, params.c_str(), sizeof(info.style));
+        } else if (name == "abv") {
+            strlcpy(info.abv, params.c_str(), sizeof(info.abv));
+        } else if (name == "ibu") {
+            strlcpy(info.ibu, params.c_str(), sizeof(info.ibu));
+        } else if (name == "link") {
+            strlcpy(info.link, params.c_str(), sizeof(info.link));
+        }
+    }
+
+    saveBeerInfo(&info);
+}
+
 static void asyncWebServerNotFoundCb(AsyncWebServerRequest *request)
 {
     request->send(404, "text/plain", "Not found");
@@ -170,9 +281,11 @@ static void setupWebServer(void)
     server.on("js/jquery.min.js", HTTP_GET, [](AsyncWebServerRequest * request) {
         request->send(FILESYSTEM, "js/jquery.min.js", "application/javascript");
     });
-    server.on("js/tbdValidate.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-        request->send(FILESYSTEM, "js/tbdValidate.js", "application/javascript");
+    server.on("js/main.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+        request->send(FILESYSTEM, "js/main.js", "application/javascript");
     });
+
+    server.on("/data", HTTP_POST, asyncWebServerDataPostCb);
 
     server.onFileUpload(asyncWebServerFileUploadCb);
 
@@ -186,24 +299,70 @@ static void setupWebServer(void)
     Serial.println("WebServer started");
 }
 
-/****************************************************************
-  ____            _              _____  _           _
- |  _ \          | |            |  __ \(_)         | |
- | |_) | __ _  __| | __ _  ___  | |  | |_ ___ _ __ | | __ _ _   _
- |  _ < / _` |/ _` |/ _` |/ _ \ | |  | | / __| '_ \| |/ _` | | | |
- | |_) | (_| | (_| | (_| |  __/ | |__| | \__ \ |_) | | (_| | |_| |
- |____/ \__,_|\__,_|\__, |\___| |_____/|_|___/ .__/|_|\__,_|\__, |
-                     __/ |                   | |             __/ |
-                    |___/                    |_|            |___/
-****************************************************************/
+
+/*****************************************************************
+  ____                   _____  _           _
+ |  _ \                 |  __ \(_)         | |
+ | |_) | ___  ___ _ __  | |  | |_ ___ _ __ | | __ _ _   _
+ |  _ < / _ \/ _ \ '__| | |  | | / __| '_ \| |/ _` | | | |
+ | |_) |  __/  __/ |    | |__| | \__ \ |_) | | (_| | |_| |
+ |____/ \___|\___|_|    |_____/|_|___/ .__/|_|\__,_|\__, |
+                                     | |             __/ |
+                                     |_|            |___/
+*****************************************************************/
+
+static void displayText(const char *str, int16_t y, uint8_t align)
+{
+    int16_t x = 0;
+    int16_t x1 = 0, y1 = 0;
+    uint16_t w = 0, h = 0;
+    display.setCursor(x, y);
+    display.getTextBounds(str, x, y, &x1, &y1, &w, &h);
+    // @TODO: lower fontsize if too wide
+    //if (w > 120) {
+    //  display.setFont();
+    //  display.getTextBounds(str, x, y, &x1, &y1, &w, &h);
+    //}
+    switch (align) {
+    case GxEPD_ALIGN_RIGHT:
+        display.setCursor(display.width() - w - x1, y);
+        break;
+    case GxEPD_ALIGN_LEFT:
+        display.setCursor(0, y);
+        break;
+    case GxEPD_ALIGN_CENTER:
+        display.setCursor(display.width() / 2 - ((w + x1) / 2), y);
+        break;
+    default:
+        break;
+    }
+    display.println(str);
+}
 
 void showBeerImage(void)
 {
     display.setRotation(0);
     display.fillScreen(GxEPD_WHITE);
-    drawBitmap(display, DEFAULT_IMAGE_BMP, 0, 0, false);
+    drawBitmap(display, DEFAULT_IMAGE_BMP, 0, 0, true);
     display.update();
 }
+
+
+void showBeerInfo(void)
+{
+    display.setRotation(0);
+    display.fillScreen(GxEPD_WHITE);
+    display.setFont(&FreeSerif18pt7b);
+    displayText(info.beername, 30, GxEPD_ALIGN_CENTER);
+    displayText(info.style, 65, GxEPD_ALIGN_CENTER);
+    display.setFont(&FreeSerifBold12pt7b);
+    displayText(info.abv, 95, GxEPD_ALIGN_CENTER);
+    displayText(info.ibu, 120, GxEPD_ALIGN_CENTER);
+    display.setFont(DEFAULT_FONT);
+    createQrCode(display, info.link, 0, 135);
+    display.update();
+}
+
 
 
 /****************************************************************
@@ -235,7 +394,15 @@ void setup()
         FILESYSTEM.begin(true);
     }
 
-    showBeerImage();
+    if (!loadBeerInfo(&info)) {
+        loadDefaultInfo();
+    }
+
+    if (SHOW_IMAGE) {
+        showBeerImage();
+    } else {
+        showBeerInfo();
+    }
 
     setupWiFi(START_WIFI_AP);
 
